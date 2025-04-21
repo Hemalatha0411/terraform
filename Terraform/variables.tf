fb81b0ba449db37b5c398e6729635b82cbc1f51e
Hemalatha0411 <140187972+Hemalatha0411@users.noreply.github.com>
2025-04-21 16:13:00 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,18 +1,8 @@
-variable "kubernetes_version" {
-  default     = 1.32
-  description = "kubernetes version"
+variable "cluster-name" {
+  default = "levelup-tf-eks-demo"
+  type    = string
 }
 
-variable "vpc_cidr" {
-  default     = "10.242.0.0/16"
-  description = "default CIDR range of the VPC"
-}
-variable "aws_region" {
+variable "AWS_REGION" {
   default = "us-east-1"
-  description = "aws region"
-}
-variable "bucket_name" {
-  type        = string
-  description = "Test bucket"
-  default     = "cmm-poc-dev"
 }
