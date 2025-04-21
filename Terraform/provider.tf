5c3840fc03c3aaaf4c006df1c8db5ab20f81311c
Hemalatha0411 <140187972+Hemalatha0411@users.noreply.github.com>
2025-04-21 16:21:58 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,13 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+
+provider "aws" {
+  region = var.AWS_REGION
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = var.aws_region
+data "aws_region" "current" {
+}
+
+data "aws_availability_zones" "available" {
+}
+
+provider "http" {
 }
