df1267199c6e7658cfae011468d9e07f4f1aed1c
Hemalatha0411 <140187972+Hemalatha0411@users.noreply.github.com>
2025-04-29 17:57:08 +0530

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -16,17 +16,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
-}
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
+  region = var.region
 }
 
 data "aws_eks_cluster" "eks" {
