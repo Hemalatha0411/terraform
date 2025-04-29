98a756c2484e7c5cbee6de091714b9d2d13e8ee8
Hemalatha0411 <140187972+Hemalatha0411@users.noreply.github.com>
2025-04-29 18:35:18 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "eks_nodes" {
+  name        = var.eks-nodes-sg
+  description = "Security group for EKS nodes"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    self            = true
+  }
+
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 10250
+    to_port         = 10250
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
